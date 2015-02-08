ac5bdcf187ac39a2dbad5d577604bfc80b991f0a
Kimmo Ilppola <kimmo.ilppola@futurice.com>
2015-02-08 12:19:15 +0200

    First working concept. Added style attributes for implementing more advanced use cases.

--- fontloader/src/main/java/com/futurice/fontloader/FontLoader.java
+++ fontloader/src/main/java/com/futurice/fontloader/FontLoader.java
@@ -1,10 +1,17 @@
 package com.futurice.fontloader;
 
+import android.content.Context;
+import android.graphics.Typeface;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by kilp on 08/02/15.
  */
 public class FontLoader {
 
+    private Map<String, Typeface> typefaceByFilename = new HashMap<>();
     private static FontLoader ourInstance = new FontLoader();
 
     public static FontLoader getInstance() {
@@ -14,4 +21,13 @@ public class FontLoader {
     private FontLoader() {
     }
 
+    public Typeface getTypeFace(Context context, String fontFile) {
+        Typeface typeface = typefaceByFilename.get(fontFile);
+        if (typeface == null) {
+            // todo: error handling
+            typeface = Typeface.createFromAsset(context.getAssets(), fontFile);
+            typefaceByFilename.put(fontFile, typeface);
+        }
+        return typeface;
+    }
 }
--- fontloader/src/main/java/com/futurice/fontloader/TextView.java
+++ fontloader/src/main/java/com/futurice/fontloader/TextView.java
@@ -2,6 +2,7 @@ package com.futurice.fontloader;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Build;
 import android.util.AttributeSet;
 
@@ -16,18 +17,38 @@ public class TextView extends android.widget.TextView {
 
     public TextView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        readAttributes(context, attrs);
     }
 
     public TextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        readAttributes(context, attrs);
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     public TextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        readAttributes(context, attrs);
     }
 
-    private void readAttributes(AttributeSet attributeSet) {
+    private void readAttributes(Context context, AttributeSet attrs) {
+        TypedArray styledAttributes = context.obtainStyledAttributes(attrs, R.styleable.TextView);
+
+        String fontFile = null;
+
+        final int N = styledAttributes.getIndexCount();
+        for (int i = 0; i < N; ++i) {
+            int attr = styledAttributes.getIndex(i);
+            if (R.styleable.TextView_fontFile == attr) {
+                fontFile = styledAttributes.getString(attr);
+            }
+        }
+
+        styledAttributes.recycle();
+
+        if (fontFile != null) {
+            this.setTypeface(FontLoader.getInstance().getTypeFace(context,fontFile));
+        }
 
     }
 
